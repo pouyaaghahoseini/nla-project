7f60c5dddc2861187e057c1cf25af8c0a69dd2df
pouyaaghahoseini <pouyaaghahoseini@gmail.com>
2020-01-23 21:10:59 +0330

    Add Problems

--- Problem 1/gauss-elimination.py
+++ Problem 1/gauss-elimination.py
@@ -1,1 +1,14 @@
 import numpy as np
+
+
+def create_tridiag(a, b, c, n):
+    A = np.zeros((n, n))
+    for i in range(n):
+        A[i][i] = b
+        if i<n-1:
+            A[i + 1][i] = a
+            A[i][i + 1] = c
+    return A
+
+x = create_tridiag(1,4,-1,50)
+print(x)
